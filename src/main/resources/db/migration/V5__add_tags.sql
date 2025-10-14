9829ef0a9ed09e48780a879e99cd907d3e0f6c4e
Boris <boris.alexandrov@custom-built-apps.com>
2025-10-14 18:08:55 -0400

    corrected the tables structure and rerun the migration

--- src/main/resources/db/migration/V5__add_tags.sql
+++ src/main/resources/db/migration/V5__add_tags.sql
@@ -1,6 +1,6 @@
 create table tags
 (
-    id bigint primary key ,
+    id bigint primary key auto_increment,
     name varchar(255),
     constraint user_tags_tags_id_fk
         foreign key (id) references users (id)
@@ -10,8 +10,9 @@ create table user_tags
 (
     user_id bigint not null ,
     tag_id bigint  not null,
+    primary key (user_id,tag_id),
     constraint user_tags_users_id_fk
-        foreign key (user_id) references users (id),
+        foreign key (user_id) references users (id) on delete cascade ,
     constraint tags_users_id_fk
-        foreign key (tag_id) references tags (id)
+        foreign key (tag_id) references tags (id) on delete cascade
 );
