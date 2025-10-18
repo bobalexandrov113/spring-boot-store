b44718d3727d9e8e2af0c7b31bf1e3ca7f74520f
Boris <boris.alexandrov@custom-built-apps.com>
2025-10-18 16:12:30 -0400

    returning DTOs manually mapped

--- src/main/java/com/cba/store/controllers/UserController.java
+++ src/main/java/com/cba/store/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.cba.store.controllers;
 
+import com.cba.store.dtos.UserDto;
 import com.cba.store.entities.User;
 import com.cba.store.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +21,29 @@ public class UserController {
     private UserRepository repository;
 
     @GetMapping
-    public List<User> findAll()
+    public ResponseEntity<List<UserDto>> findAll()
     {
-       List<User> list = new ArrayList<>();
-       for (User user : repository.findAll())
-       {
-           list.add(user);
-       }
+       List<UserDto> list =repository
+               .findAll()
+               .stream()
+               .map(user->new UserDto(user.getId(), user.getName(), user.getEmail()))
+               .toList();
 
-       return list;
+       if(list.isEmpty())
+           return ResponseEntity.notFound().build();
+       else return ResponseEntity.ok().body(list);
 
     }
 
     @GetMapping("/{id}")
-    public User findUserById(@PathVariable Long id)
+    public ResponseEntity<UserDto> findUserById(@PathVariable Long id)
     {
        User user = repository.findById(id).orElse(null);
-
-        return user;
-
+       if (user == null)
+           return ResponseEntity.notFound().build();
+       else {
+           UserDto userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+           return ResponseEntity.ok(userDto);
+       }
     }
 }
--- src/main/java/com/cba/store/repositories/UserRepository.java
+++ src/main/java/com/cba/store/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.cba.store.repositories;
 
 import com.cba.store.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long>
+
+public interface UserRepository extends JpaRepository<User, Long>
 {
 }
