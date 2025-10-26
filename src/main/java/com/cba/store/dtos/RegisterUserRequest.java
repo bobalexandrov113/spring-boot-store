6a7e7104b5c647491d1a6bc7b658aeccbecff808
Boris <boris.alexandrov@custom-built-apps.com>
2025-10-25 23:17:48 -0400

    added custom annotation validator for lower case field

--- src/main/java/com/cba/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/cba/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.cba.store.dtos;
 
+import com.cba.store.validation.Lowercase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,12 +9,13 @@ import lombok.Data;
 @Data
 public class RegisterUserRequest {
     @NotBlank(message = "Name is required")
-    @Size(message = "Name must be less than 255 characters")
+    @Size(max=255,message = "Name must be less than 255 characters")
     private String name;
     @Email(message = "Email must be valid")
     @NotBlank(message = "Email is required")
+    @Lowercase(message = "email has to be in lowercase")
     private String email;
     @NotBlank(message = "Password is required")
-    @Size(message = "Passworkd must be between 1 and 6 characters")
+    @Size(min=1,max=6,message = "Password must be between 1 and 6 characters")
     private String password;
 }
