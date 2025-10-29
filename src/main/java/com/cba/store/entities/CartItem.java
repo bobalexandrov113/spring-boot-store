80779fa7de10af8d021113cb45ec463510d79a69
Boris <boris.alexandrov@custom-built-apps.com>
2025-10-29 19:48:21 -0400

    cart entities

---
+++ src/main/java/com/cba/store/entities/CartItem.java
@@ -0,0 +1,32 @@
+package com.cba.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_items", schema = "store", uniqueConstraints = {
+        @UniqueConstraint(name = "cart_items_unique", columnNames = {"product_id", "cart_id"})
+})
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+
+    @ManyToOne()
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+}
