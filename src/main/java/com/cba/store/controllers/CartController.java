19ba64c1ebc204dc061e8cfd936c9a2e61ffc44d
Boris <boris.alexandrov@custom-built-apps.com>
2025-11-02 10:23:54 -0500

    delete cart item using product id

--- src/main/java/com/cba/store/controllers/CartController.java
+++ src/main/java/com/cba/store/controllers/CartController.java
@@ -100,4 +100,24 @@ public class CartController {
         return ResponseEntity.ok(cartMapper.toDto(cartItem));
 
     }
+
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<?> removeItem(
+            @PathVariable UUID cartId,
+            @PathVariable Long productId
+    )
+    {
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "Cart not found")
+            );
+        }
+            cart.removeItem(productId);
+            cartRepository.save(cart);
+            return ResponseEntity.noContent().build();
+    }
+
+
 }
