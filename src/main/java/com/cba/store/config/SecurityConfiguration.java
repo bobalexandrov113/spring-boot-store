d77705d6769387587f83c0c80ebf831d07f9a74b
Boris <boris.alexandrov@custom-built-apps.com>
2025-11-04 11:54:31 -0500

    created authentication manager, tested with /auth/login

--- src/main/java/com/cba/store/config/SecurityConfiguration.java
+++ src/main/java/com/cba/store/config/SecurityConfiguration.java
@@ -1,21 +1,31 @@
 package com.cba.store.config;
 
 
+import com.cba.store.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 
 public class SecurityConfiguration {
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -34,4 +44,18 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
+
--- src/main/java/com/cba/store/repositories/UserRepository.java
+++ src/main/java/com/cba/store/repositories/UserRepository.java
@@ -3,10 +3,12 @@ package com.cba.store.repositories;
 import com.cba.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
     boolean existsByEmail(String email);
 
-   User findByEmail(String email);
+   Optional<User> findByEmail(String email);
 }
