bee9bd63abe3b4de81988c86b8199ce8116abfcf
Boris <boris.alexandrov@custom-built-apps.com>
2025-11-03 20:39:39 -0500

    added configuration, verified carts are working. others fail without passwordencoder

---
+++ src/main/java/com/cba/store/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.cba.store.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfiguration {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(c->c.
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(a->a
+                .requestMatchers("/carts/**","/login","/","/error").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
