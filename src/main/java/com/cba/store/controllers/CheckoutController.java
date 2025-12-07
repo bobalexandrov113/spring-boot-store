d6ac3a36f0af26929106d4f79cbb589771717cb7
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-07 05:24:02 -0500

    added ErrorDto to send as response instead of Map.of(error,message)

--- src/main/java/com/cba/store/controllers/CheckoutController.java
+++ src/main/java/com/cba/store/controllers/CheckoutController.java
@@ -1,8 +1,11 @@
 package com.cba.store.controllers;
 
+import com.cba.store.dtos.CartDto;
 import com.cba.store.dtos.CheckoutRequest;
 import com.cba.store.dtos.CheckoutResponse;
+import com.cba.store.dtos.ErrorDto;
 import com.cba.store.repositories.CartRepository;
+import com.cba.store.services.CartService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 @RestController
@@ -18,16 +22,24 @@ import java.util.Map;
 
 @AllArgsConstructor
 public class CheckoutController {
-    private CartRepository cartRepository;
+    private CartService cartService;
 
     @PostMapping
     public ResponseEntity<?> checkout(
          @Valid @RequestBody CheckoutRequest request)
     {
-       var cart = cartRepository.findById(request.getCartId());
-       if (cart == null) {
+        CartDto cart ;
+      try {
+           cart = cartService.getCart(request.getCartId());
+      }
+      catch (Exception e) {
+          //return ResponseEntity.badRequest().body(Map.of("error","Cart not found"));
+          return ResponseEntity.badRequest().body(new ErrorDto("Cart not found"));
+      }
+      if( cart.getTotalPrice().compareTo(BigDecimal.ZERO)==0)
+       {
            return ResponseEntity.badRequest().body(
-                   Map.of("error","Cart not found")
+                   new ErrorDto("Cart is empty")
            );
        }
        return ResponseEntity.ok(cart);
