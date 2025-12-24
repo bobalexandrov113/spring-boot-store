8ced68ca76c8fa8d449f85e0e798589061e015d4
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-24 10:46:50 -0500

    removed carts exclusion, put swagger in its own line only

--- src/main/java/com/cba/store/config/SecurityConfiguration.java
+++ src/main/java/com/cba/store/config/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration {
                 sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(a->a
-                .requestMatchers("/carts/**","/login","/","/error/**","/swagger-ui.html","/swagger-ui/**").permitAll()
+                .requestMatchers("/login","/","/error/**").permitAll()
                 .requestMatchers("/swagger-ui.html","/swagger-ui/**","/v3/api-docs/**").permitAll()
                 .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                 .requestMatchers("/users/**").hasRole(Role.ADMIN.name())
