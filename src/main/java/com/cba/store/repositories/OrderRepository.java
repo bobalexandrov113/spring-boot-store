f45a6e8a7a84fba10799351c7a9aa8368e582c61
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-25 13:04:18 -0500

    added the EntityGraph to the orders to minimize the queries

--- src/main/java/com/cba/store/repositories/OrderRepository.java
+++ src/main/java/com/cba/store/repositories/OrderRepository.java
@@ -1,14 +1,18 @@
 package com.cba.store.repositories;
 
-import com.cba.store.dtos.OrderDto;
 import com.cba.store.entities.Order;
 import com.cba.store.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long>
 {
 
-    List<Order> findAllByCustomer(User customer);
+   @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT o from Order o WHERE o.customer=:customer")
+    List<Order> getAllByCustomer(@Param("customer") User customer);
 }
