7d6787011df4175a3640bb00d44db77fe27b1cb5
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-25 10:34:31 -0500

    Added exception handler to convey the default message

--- src/main/java/com/cba/store/controllers/CheckoutController.java
+++ src/main/java/com/cba/store/controllers/CheckoutController.java
@@ -5,6 +5,8 @@ import com.cba.store.dtos.CheckoutRequest;
 import com.cba.store.dtos.CheckoutResponse;
 import com.cba.store.dtos.ErrorDto;
 import com.cba.store.entities.*;
+import com.cba.store.exceptions.CartEmptyException;
+import com.cba.store.exceptions.CartNotFoundException;
 import com.cba.store.mappers.ProductMapper;
 import com.cba.store.repositories.CartRepository;
 import com.cba.store.repositories.OrderRepository;
@@ -13,11 +15,9 @@ import com.cba.store.services.CartService;
 import com.cba.store.services.CheckoutService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.Map;
@@ -31,10 +31,14 @@ public class CheckoutController {
     private CheckoutService checkoutService;
 
     @PostMapping
-    public ResponseEntity<?> checkout(
+    public CheckoutResponse checkout (
     @Valid @RequestBody CheckoutRequest request)
     {
-       var checkoutResponse = checkoutService.checkout(request);
-       return ResponseEntity.ok(checkoutResponse);
+       return checkoutService.checkout(request);
+    }
+    @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
+    public ResponseEntity<ErrorDto> handleException(Exception ex)
+    {
+        return ResponseEntity.badRequest().body(new ErrorDto(ex.getMessage()));
     }
 }
