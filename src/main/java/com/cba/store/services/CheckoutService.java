9215edab3656226868f316c5189e6da4f897d8fe
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-31 13:18:00 -0500

    added final so that required args constructor annotation did work

--- src/main/java/com/cba/store/services/CheckoutService.java
+++ src/main/java/com/cba/store/services/CheckoutService.java
@@ -4,21 +4,29 @@ import com.cba.store.dtos.CheckoutRequest;
 import com.cba.store.dtos.CheckoutResponse;
 import com.cba.store.entities.Order;
 import com.cba.store.exceptions.CartEmptyException;
+import com.cba.store.exceptions.CartNotFoundException;
 import com.cba.store.repositories.OrderRepository;
+
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
-    public CartService cartService;
-    private AuthService authService;
-    private OrderRepository orderRepository;
+    private final CartService cartService;
+    private final AuthService authService;
+    private final OrderRepository orderRepository;
 
 
     public CheckoutResponse checkout(CheckoutRequest request) {
 
         var cart = cartService.getCart(request.getCartId());
+
+        if (cart == null) {
+            throw new CartNotFoundException("Cart not found");
+        }
 
         if( cart.isEmpty()) {
             throw new CartEmptyException();
