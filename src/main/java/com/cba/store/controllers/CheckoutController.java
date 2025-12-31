730a90ef9913c00e4eb5f5e66c656c33fbf8abbe
Boris <boris.alexandrov@custom-built-apps.com>
2025-12-31 14:10:33 -0500

    changed the return type and handling of the Stripe exception

--- src/main/java/com/cba/store/controllers/CheckoutController.java
+++ src/main/java/com/cba/store/controllers/CheckoutController.java
@@ -6,8 +6,10 @@ import com.cba.store.dtos.ErrorDto;
 import com.cba.store.exceptions.CartEmptyException;
 import com.cba.store.exceptions.CartNotFoundException;
 import com.cba.store.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +21,13 @@ public class CheckoutController {
     private CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout (
-    @Valid @RequestBody CheckoutRequest request)
-    {
-       return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout (
+    @Valid @RequestBody CheckoutRequest request) {
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (StripeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDto(e.getMessage()));
+        }
     }
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
     public ResponseEntity<ErrorDto> handleException(Exception ex)
