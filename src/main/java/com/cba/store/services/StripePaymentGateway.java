d0770d2dc29ba06e09ac33ffad24ce3fd2b4193f
Boris <boris.alexandrov@custom-built-apps.com>
2026-01-01 12:28:41 -0500

    added order id as metadata to send to the Stripe server

--- src/main/java/com/cba/store/services/StripePaymentGateway.java
+++ src/main/java/com/cba/store/services/StripePaymentGateway.java
@@ -27,7 +27,8 @@ public class StripePaymentGateway implements PaymentGateway
             var builder = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?order_id=" + order.getId())
-                    .setCancelUrl(websiteUrl + "/checkout-cancel?order_id=" + order.getId());
+                    .setCancelUrl(websiteUrl + "/checkout-cancel")
+                    .putMetadata("order_id",order.getId().toString());
 
             order.getItems().forEach(orderItem -> {
                 var lineItem = createLineItem(orderItem);
