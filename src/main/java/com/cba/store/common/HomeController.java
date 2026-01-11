39c99ffe01d10448b7b343d93b5b8edaf657586a
Boris <boris.alexandrov@custom-built-apps.com>
2026-01-11 09:44:18 -0500

    added env variable to application yaml files to govern the html/js returned by the home controller

--- src/main/java/com/cba/store/common/HomeController.java
+++ src/main/java/com/cba/store/common/HomeController.java
@@ -1,15 +1,26 @@
 package com.cba.store.common;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Value("${env}")
+
+    private String env;
+
     @GetMapping("/")
     public String home(Model model)
         {
             model.addAttribute("name", "Visitor");
+        if (env.equals("production")) {
+            return "index";
+        }
+        else if (env.equals("development")) {
+            return "index_dev";
+        }
         return "index";
         }
 
